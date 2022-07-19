d005999661f1e5c024cf8963657123b62b824084
antonyMeow <103539687+antonyMeow@users.noreply.github.com>
2022-07-19 12:29:21 +0300

    delete tests

--- tests/getLunarPhaseTest.js
+++
@@ -1,9 +0,0 @@
-const randomDate = require("./randomDate");
-const { getLunarDay, getLunarPhase } = require("../src/calculations/getLunarPhase");
-
-for (let i = 0; i < 500; i++) {
-  const date = randomDate();
-  const lunarDay = getLunarDay(date);
-  const lunarPhase = getLunarPhase(lunarDay);
-  process.stdout.write(`${date}: ${lunarDay}: ${lunarPhase} \n`);
-}
--- tests/randomDate.js
+++
@@ -1,9 +0,0 @@
-function randomDate() {
-  const startDate = new Date(Date.UTC(0, 0, 1));
-  const endDate = new Date(Date.UTC(5000, 12, 30));
-  const startTime = startDate.getTime();
-  const endTime = endDate.getTime();
-  return new Date(startTime + Math.random() * (endTime - startTime));
-}
-
-module.exports = randomDate;
